0769b158cf3e12b81894b5417da4e1a571b100cc
Tinatin Tsagurishvili <ttsagurishvili@credo.ge>
2024-10-23 23:49:58 +0400

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Channels;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "Tiko";
+        Console.WriteLine(name + "Tsagura");
+        string name1 = "Tinatin";
+        string name2 = "Tsagurishvili";
+        Console.WriteLine(name1 + name2);
+        byte b1 = 38;
+        string name3 = "Age";
+        Console.WriteLine(name3 + b1);
+        int b2 = 172;
+        string name4 = "Height";
+        Console.WriteLine(name4 + b2);
+        byte b3 = 60;
+        string name5 = "Weight";
+        Console.WriteLine(name4 + b3);
+        string name6 = "isMarried";
+        bool isMarried = true;
+        Console.WriteLine(name6 + isMarried);
+
+    }
+}
