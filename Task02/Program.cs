c3c7f39329f7ec4249239c3c2822b7279a0aea2b
Tinatin Tsagurishvili <ttsagurishvili@credo.ge>
2024-10-25 00:30:38 +0400

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Random random = new();
+int randomNumber = random.Next(1, 10);
+int maxTries = 3;
+
+for(int i = 1; i <= maxTries; i++)
+{
+    Console.WriteLine("Input Number");
+    string input = Console.ReadLine();
+    int inputNum = int.Parse(input); //convert to int
+
+    if (randomNumber == inputNum)
+    {
+        Console.WriteLine("You Guessed the number");
+        break;
+    }
+    else if (i == maxTries)
+    {
+        Console.WriteLine("Game Over");
+        break;
+    }
+        
+    Console.WriteLine("You have: "+(maxTries - i)+" Tries Left");
+}
