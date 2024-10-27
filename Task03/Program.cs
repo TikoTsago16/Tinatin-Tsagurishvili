01607e44a7cd9ae9313060c188d63b5e84522c90
Tinatin Tsagurishvili <ttsagurishvili@credo.ge>
2024-10-28 01:29:53 +0400

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,68 @@
+﻿
+Console.Write("Enter a number from 1 to 7: ");
+
+string input = Console.ReadLine();
+
+
+if (int.TryParse(input, out int dayNumber))
+
+{
+
+  
+    switch (dayNumber)
+
+    {
+
+        case 1:
+
+            Console.WriteLine("orshabati");
+
+            break;
+
+        case 2:
+
+            Console.WriteLine("samshabati");
+
+            break;
+
+        case 3:
+
+            Console.WriteLine("otxshabati");
+
+            break;
+
+        case 4:
+
+            Console.WriteLine("xutshabati");
+
+            break;
+
+        case 5:
+
+            Console.WriteLine("paraskevi");
+
+            break;
+
+        case 6:
+
+            Console.WriteLine("shabati");
+
+            break;
+
+        case 7:
+
+            Console.WriteLine("kvira");
+
+            break;
+
+        default:
+
+            Console.WriteLine("Invalid digit. Please try again from 1 to 7");
+
+            break;
+    }
+}
+
+   
+
+
