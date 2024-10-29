450c2e264da363e6ce5a88b60a84e48163fe4f0f
Tinatin Tsagurishvili <ttsagurishvili@credo.ge>
2024-10-29 20:52:27 +0400

    Task03c

---
+++ Task03c/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("sheikvanet ricxvi: ");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int number))
+{
+    Console.WriteLine($"\ngamrvlebis cxrili {number}:");
+    Console.WriteLine("-------------------------");
+
+    // for ციკლით გამოვიტანო გამრავლების ცხრილი 1-დან 10-მდე
+    for (int i = 1; i <= 10; i++)
+    {
+        int result = number * i;
+        Console.WriteLine($"{number} x {i} = {result}");
+    }
+
+    Console.WriteLine("-------------------------");
+}
+else
+{
+    Console.WriteLine("gtxovt sheikvanot mxolod ricxvi!");
+}
