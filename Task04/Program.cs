f10ea4f9569c6cd899f861b7d037ab177fdb7a12
Tinatin Tsagurishvili <ttsagurishvili@credo.ge>
2024-10-29 21:08:15 +0400

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,23 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        
+        string[] students = new string[3];
+
+        Console.WriteLine("Enter the names of 3 students:");
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            Console.Write($"Student {i + 1}: ");
+            students[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("\nList of Students:");
+        foreach (string student in students)
+        {
+            Console.WriteLine(student);
+        }
+    }
+}
