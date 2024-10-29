37911d6d5b90a04531225777b49adb81e4133f91
Tinatin Tsagurishvili <ttsagurishvili@credo.ge>
2024-10-29 20:28:46 +0400

    Task03b

---
+++ Task03b/Program.cs
@@ -0,0 +1,19 @@
+﻿int sum = 0;
+int number;
+
+Console.WriteLine("sheikvanet dadebiti ricxvi (uarkopiti ricxvi daasrulebs programas):");
+
+// წავიკითხოთ პირველი რიცხვი
+string input = Console.ReadLine();
+
+// შევამოწმოთ არის თუ არა შეყვანილი მნიშვნელობა რიცხვი
+while (int.TryParse(input, out number) && number >= 0)
+{
+    // დავამატოთ რიცხვი ჯამს
+    sum += number;
+
+    // წავიკითხოთ შემდეგი რიცხვი
+    input = Console.ReadLine();
+}
+
+Console.WriteLine($"jami: {sum}");
