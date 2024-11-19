b1b271ab6a072194700fd2b0739c9454c9458460
Tinatin Tsagurishvili <ttsagurishvili@credo.ge>
2024-11-20 00:26:56 +0400

    Task11(2)

---
+++ Task11(2)/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+static class MathHelper
+{
+   
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    
+    public static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+   
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+   
+    public static double Divide(int a, int b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Division by zero is impossible.");
+        }
+        return (double)a / b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int a = 20;
+        int b = 5;
+
+        
+        Console.WriteLine("Add: " + MathHelper.Add(a, b));
+        Console.WriteLine("Subtract: " + MathHelper.Subtract(a, b));
+        Console.WriteLine("Multiply: " + MathHelper.Multiply(a, b));
+
+        try
+        {
+            Console.WriteLine("Divide: " + MathHelper.Divide(a, b));
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        
+        b = 0;
+        try
+        {
+            Console.WriteLine("Divide with b=0: " + MathHelper.Divide(a, b));
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
