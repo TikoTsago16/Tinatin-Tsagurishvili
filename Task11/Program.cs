77cadd24334cd82a38470ec95e650e29814ba328
Tinatin Tsagurishvili <ttsagurishvili@credo.ge>
2024-11-20 00:03:21 +0400

    Task11(1)

---
+++ Task11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Counter
+{
+   
+    private static int Count = 0;
+
+  
+    public Counter()
+    {
+        Count++;
+    }
+
+    public static int GetCount()
+    {
+        return Count;
+    }
+
+    public static void ResetCount()
+    {
+        Count = 0;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        
+        Counter c1 = new Counter();
+        Console.WriteLine($"Current count after creating c1: {Counter.GetCount()}"); 
+
+        Counter c2 = new Counter();
+        Console.WriteLine($"Current count after creating c2: {Counter.GetCount()}"); 
+
+        
+        Counter c3 = new Counter();
+        Console.WriteLine($"Current count after creating c3: {Counter.GetCount()}"); 
+
+        // Count-ის reset
+        Counter.ResetCount();
+        Console.WriteLine($"Count after reset: {Counter.GetCount()}"); 
+    }
+}
